05943233532ee5f39254447f781741b7b02cc75c
Jennifer Cardenas <rc.cardenashernandez.jennifer@leadps.org>
2016-11-17 18:17:46 +0000

    Added the loop examples

---
+++ class_samples/3-5_looppractice/parking_fines.py
@@ -0,0 +1,11 @@
+months = 0
+ticket = 60
+
+while ticket > 0:
+	print("Have you paid your ticket? yes/no")
+	response = raw_input()
+	if response == "yes ":
+		ticket = 0 
+	else: 
+		ticket = ticket * 2
+	print("OK, your ticket is now " + str(ticket))
