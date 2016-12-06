c1fc9e058faf193e894f1cc2ffbb8c69741374d9
Jennifer Cardenas <rc.cardenashernandez.jennifer@leadps.org>
2016-12-06 18:08:43 +0000

    new update to quest.py

--- problem_sets/quest.py
+++ problem_sets/quest.py
@@ -3,9 +3,10 @@
 import random
 # creates a random number between 3 and 567 - edit as necessary
 myNum = random.randint(1, 1000)
-
+#sets jenny to 0 and asks you to enter a # between 1 and 1000
 jenny = 0
 print("I\'m thinking of a number between  1 and 1000. Enter your guess!")
+#it says while jenny is less than myNum it tests the choice until you guess the right number
 while jenny < myNum:
 	choice = int(raw_input())
 	if choice > myNum:
@@ -14,4 +15,5 @@ while jenny < myNum:
 		print("That number is too low! Guess again.")
 	elif choice == myNum:
 		print("Hooray, you won!")
+#it assigns jenny to a high # so the loop will terminate when it isin't true anymore
 jenny = 10000000
